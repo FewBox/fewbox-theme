0bd352c6783815f0d2f758f31ce9e8e4c3f4a944
landpy <landpy@outlook.com>
2020-03-29 19:47:32 +0800

    Init color.

--- index.ts
+++ index.ts
@@ -1,3 +1,23 @@
 module.exports = {
-  'primary-color': '#1DA57A',
+  // Base
+  'primary-color': '#7c7a6a',
+  'primary-variant-color': '#17280a',
+  'secondary-color': '#e1e0da',
+  'secondary-variant-color': '#b7bea8',
+  'background-color': '#ffffff',
+  'surface-color': '#b3b3b3',
+  'error-color': '#ff0000',
+  // On
+  'on-primary-color': '#adab9f',
+  'on-primary-variant-color': '#2f5214',
+  'on-secondary-color': '#f4f3f1',
+  'on-secondary-variant-color': '#dbded3',
+  'on-background-color': '#e6e6e6',
+  'on-surface-color': '#808080',
+  'on-error-color': '#330000',
+  // Other
+  'typography-color': '#b4b3b4',
+  'iconography-color': '#000000',
+  'accessible-color': '#000000',
+  'alternative-color': '#000000',
 };
